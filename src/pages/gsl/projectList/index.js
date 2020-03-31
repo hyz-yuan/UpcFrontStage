c90969ca0f420ba41bc396e02afa22f0275f5965
ljhang521 <ljhang521@sina.com>
2020-03-31 11:29:00 +0800

    test

---
+++ src/pages/gsl/projectList/index.js
@@ -0,0 +1,135 @@
+import * as React from "react";
+import {Table, Input, Button, Modal, Form} from "antd";
+import {fetchPost} from "../../../static/util/fetch";
+import {createHashHistory} from "history";
+import RightBodyHeaderBar from '../../../static/component/rightBodyHeaderBar'
+import moment from "moment";
+
+
+class projectList extends React.Component {
+
+    state = {
+        projectId: null,
+        title: "项目列表",
+        searchValue: '',
+        columns: [
+            {title: '序号', dataIndex: '', render: (text, record, index) => `${index + 1}`},
+            {
+                title: '项目名称', dataIndex: 'projectName',
+                //点击项目名称后，进行跳转
+                render: (text, record) => (<a onClick={(e) => this.goChildren(record.id)}>{text}</a>)
+            },
+            {title: '实施地', dataIndex: 'workPlaceName',},
+            {title: '负责人', dataIndex: 'managerName',},
+            {title: '开始时间', dataIndex: 'beginTime',},
+            {title: '结束时间', dataIndex: 'endTime',},
+            {
+                title: '操作', dataIndex: 'action',
+                render: (text, record) => <span>
+        <a name={"changeProject"} onClick={() => this.handleProjectManage(record.id)}>修改{record.name}</a>&nbsp;&nbsp;
+                    <a onClick={() => this.showModal(record.id)}>删除</a>
+      </span>
+            }
+        ],
+        data: [],
+        requestLoading: true,
+        visible: false
+    };
+    //修改项目
+    handleProjectManage = (id) => {
+        createHashHistory().push('/sys/projectManage/'+id)
+    };
+
+    goChildren = (id) => {
+        sessionStorage.clear();
+        createHashHistory().push('/sys/projectDetail'+id)
+    };
+
+
+    componentDidMount = (value) => {
+        let params = {projectName: value};
+        fetchPost(global.constants.projectList, params)
+            .then(
+                res => this.setData(res)
+            )
+            .catch(e => console.log(e))
+            .finally(() => {
+                this.setState({
+                    requestLoading: false
+                })
+            })
+    };
+
+    //赋予表格数据
+    setData = (list) => {
+        this.setState({
+            data: list.map((item, index) => {
+                return {
+                    ...item,
+                    beginTime: moment(item.beginTime).format("YYYY-MM-DD"),
+                    endTime: moment(item.endTime).format("YYYY-MM-DD"),
+                    key: index
+                }
+            })
+        })
+    };
+
+    showModal = (id) => {
+        this.setState({
+            visible: true, projectId: id
+        });
+    };
+
+
+    //删除的弹出框的确认按钮,执行删除操作
+    handleOk = e => {
+        console.log(e);
+        let params = {id: this.state.projectId};
+        fetchPost(global.constants.deleteProject, params).then(
+            res => this.setData(res)
+        )
+            .catch(e => console.log(e))
+            .finally(() => {
+                this.setState({
+                    requestLoading: false
+                })
+            });
+        this.setState({
+            visible: false,
+        });
+    };
+
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+
+
+    render() {
+        const {data, columns, title} = this.state;
+        const {Search} = Input;
+        return (
+            <div>
+                <RightBodyHeaderBar title={title}/>
+                <Search addonBefore={"项目查询"} placeholder="请输入项目名称" enterButton="查询"
+                        style={{width: 400}}
+                        onSearch={value => this.componentDidMount(value)}/>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <Button type="primary" name={"addProject"} onClick={()=>this.handleProjectManage(0)}>新增项目</Button>
+                <Table dataSource={data} pagination={{pageSize: 5}} columns={columns}/>
+                <Modal
+                    title={"提示!"}
+                     visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}>
+                    <p>确认要删除这条记录吗</p>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default projectList
+
---
+++ src/pages/gsl/projectManage/index.js
@@ -0,0 +1,214 @@
+import React, {Component} from 'react'
+import {Form, Button, Input, Select, Modal} from 'antd'
+import {DatePicker} from 'antd';
+import RightBodyHeaderBar from "../../../static/component/rightBodyHeaderBar";
+import {fetchPost} from "../../../static/util/fetch";
+import {createHashHistory} from "history";
+
+import moment from 'moment';
+
+
+class projectManage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: "",
+            projectName: "",
+            place: null,
+            manager: null,
+            beginTime: "",
+            endTime: "",
+            remark: "",
+            workPlaceName: "",
+            managerName: "",
+        };
+    }
+
+
+    componentDidMount() {
+        let projectId = this.props.match.params.id;
+        console.log("项目id：" + projectId);
+        if (projectId !== "0") {
+            let params = {id: projectId};
+            fetchPost(global.constants.getProject, params)
+                .then(res => {
+                    console.log(res.id);
+                    this.setState({
+                        id: res.id,
+                        projectName: res.projectName,
+                        place: res.place,
+                        manager: res.manager,
+                        beginTime: res.beginTime,
+                        endTime: res.endTime,
+                        remark: res.remark,
+                        workPlaceName:res.workPlaceName,
+                        managerName:res.managerName,
+                    })
+                });
+        }
+    };
+
+
+    //提交项目数据
+    postProject = () => {
+        console.log(this.state);
+        let projectId = this.props.match.params.id;
+        //0为新建项目的标志，非0位修改项目的标志
+        if (projectId === "0") {
+            fetchPost(global.constants.addProject, this.state)
+                .catch(e => console.log(e))
+                .finally(() => {
+                    this.setState({
+                        requestLoading: false
+                    });
+                });
+            //提示成功
+            this.success("保存成功")
+        } else {
+            fetchPost(global.constants.changeProject, this.state)
+                .catch(e => console.log(e))
+                .finally(() => {
+                    this.setState({
+                        requestLoading: false
+                    })
+                });
+            //提示操作成功
+            this.success("修改成功");
+        }
+    };
+
+    //消息提示框
+    success = (msg) => {
+        Modal.success({
+            content: msg,
+        });
+    };
+
+    goFather = () => {
+        // sessionStorage.clear();
+        createHashHistory().push('/sys/projectList')
+    };
+
+    handleGetInputValue = (event) => {
+        this.setState({
+            projectName: event.target.value,
+        })
+    };
+
+    onChangeB = (value, dateString) => {
+        // console.log('Selected Time: ', moment(value).format('YYYY-MM-DD'));
+        // console.log('Formatted Selected Time: ', dateString);
+        this.setState({
+            beginTime: dateString,
+        })
+    };
+
+    onChangeE = (value, dateString) => {
+        this.setState({
+            endTime: dateString,
+        })
+    };
+
+    render() {
+        console.log("aaa" + this.state.projectName);
+        const layout = {
+            labelCol: {span: 5},//设置距离做边框的距离
+            wrapperCol: {span: 12},//宽度
+        };
+        const tailLayout = {
+            wrapperCol: {offset: 8, span: 12},
+        };
+
+
+        const {Option} = Select;
+
+        return (
+            <div>
+                <RightBodyHeaderBar title={"新增/修改项目"}/>
+                <Form
+                    {...layout}
+                    // initialValues={{remember: false}}
+                >
+                    <Form.Item
+                        label="项目名称"
+                        rules={[{required: true, message: '请输入项目名称!'}]}
+                    >
+                        <Input
+                            value={this.state.projectName}
+                            onChange={this.handleGetInputValue}/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="实施地点"
+                        rules={[{required: true, message: '请选择项目地点!'}]}
+                    >
+                        <Select
+                            defaultValue={Option.valueOf()}
+                            value={this.state.place}
+                            // value={this.state.workPlaceName}
+                            onChange={value => this.setState({place: value})}
+                            allowClear
+                        >
+                            <Option value="1">北京</Option>
+                            <Option value="2">东营</Option>
+                            <Option value="3">青岛</Option>
+                            <Option value="4">沈阳</Option>
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="项目负责人"
+                        // name="manager"
+                        rules={[{required: true, message: '请选择项目负责人!'}]}
+                    >
+                        <Select
+                            defaultValue={Option.valueOf()}
+                            value={this.state.manager}
+                            // value={this.state.managerName}
+                            onChange={value => this.setState({manager: value})}
+                            allowClear
+                        >
+                            <Option value="1">张三</Option>
+                            <Option value="2">c</Option>
+                            <Option value="3">ddd</Option>
+                            <Option value="4">cxm</Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="起止日期"
+                               rules={[{required: true, message: '请选择日期!'}]}
+                    >
+                        <DatePicker
+                            showTime
+                            format='YYYY-MM-DD HH:mm:ss'
+                            value={this.state.beginTime !== "" ? moment(this.state.beginTime) : null}
+                            onChange={this.onChangeB}/>
+                        —
+                        <DatePicker
+                            showTime
+                            format='YYYY-MM-DD HH:mm:ss'
+                            value={this.state.endTime !== "" ? moment(this.state.endTime) : null}
+                            onChange={this.onChangeE}/>
+                    </Form.Item>
+                    <Form.Item label="备注"
+                    >
+                        <Input value={this.state.remark}
+                               onChange={event => {
+                                   this.setState({remark: event.target.value})
+                               }}/>
+                    </Form.Item>
+                    <Form.Item {...tailLayout}>
+                        <Button onClick={() => this.goFather()}>
+                            返回
+                        </Button>&nbsp;&nbsp;
+                        <Button type="primary" onClick={() => this.postProject()}>
+                            提交
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        );
+    };
+}
+
+export default projectManage;
